6bf093e2f1982dadb097cc47510a790f797d08f8
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2022-11-11 09:40:26 +0100

    feat: create new user tillags

--- CreateNewUser.xaml.cs
+++ CreateNewUser.xaml.cs
@@ -10,27 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TheStore;
 
 
 namespace TheStore
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreateNewUser.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateNewUser : Window
     {
-        public MainWindow()
+        public CreateNewUser()
         {
             InitializeComponent();
-        }
-
-        private void logCreateButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-            CreateNewUser createNewUser = new CreateNewUser();
-            createNewUser.Show();
         }
     }
 }
