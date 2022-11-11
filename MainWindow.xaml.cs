27a0f9553a299daa8be3f9d95f5816c156233e20
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2022-11-11 12:43:44 +0100

    feat: header tar nu inlogg och döljer/visar knappar beroende på om man är inloggad eller inte

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,18 +23,52 @@ namespace TheStore
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<User> userList = new List<User>();
+
         public MainWindow()
         {
+            User user = new User("Max", "bananer", "max@max.com", "Stan", 0735040590);
+            userList.Add(user);
             InitializeComponent();
-
-         
         }
 
-        private void logCreateButton_Click(object sender, RoutedEventArgs e)
+        private void logButton_Click(object sender, RoutedEventArgs e)
         {
+
+            string mail = mailBox.Text;
+            string password = pwBox.Password;
+            
+           
+            foreach (User user in userList)
+            {
+                if (mail.Equals(user.getEmail()) && password.Equals(user.getPassword()))
+                {
+                    mailBox.Visibility = Visibility.Collapsed;
+                    pwBox.Visibility = Visibility.Collapsed;
+                    userNameText.Text = " back " + user.getName();
+                    logButton.Visibility = Visibility.Collapsed;
+                    createButton.Visibility = Visibility.Collapsed;
+                    logOutButton.Visibility = Visibility.Visible;
+                }
+            }
+
+        }
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+
             this.Hide();
             CreateNewUser createNewUser = new CreateNewUser();
             createNewUser.Show();
+        }
+
+        private void logOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            mailBox.Visibility = Visibility.Visible;
+            pwBox.Visibility = Visibility.Visible;
+            userNameText.Text = "";
+            logButton.Visibility = Visibility.Visible;
+            createButton.Visibility = Visibility.Visible;
+            logOutButton.Visibility = Visibility.Collapsed;
         }
     }
 }
