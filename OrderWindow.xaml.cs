ef56e7abecaa6535866fc1887f2c85c5790b3cbf
David Ankner <113942616+Anki337@users.noreply.github.com>
2022-11-16 08:39:16 +0100

    Add: OrderWindow design

--- OrderWindow.xaml.cs
+++ OrderWindow.xaml.cs
@@ -19,15 +19,22 @@ namespace TheStore
     /// </summary>
     public partial class OrderWindow : Window
     {
+        List<string> billOpt = new List<string>();
+
         public OrderWindow()
         {
             InitializeComponent();
-            
+            string payOne = "Klarna, Lån";
+            string payTwo = "Mastercard, Kredit";
+            string payThree = "Bankkonto, Tillgångar";
+            billOpt.Add(payOne);
+            billOpt.Add(payTwo);
+            billOpt.Add(payThree);
         }
 
         private void logCreateButton_Click(object sender, RoutedEventArgs e)
         {
-            //Send the current itemlist in shoppingcart with the constructor.
+            
             CreateNewUser createNewUser = new CreateNewUser();
             createNewUser.Show();
             this.Close();
--- PaymentOptions.cs
+++ PaymentOptions.cs
@@ -8,33 +8,18 @@ namespace TheStore
 {
     public class PaymentOptions
     {
+
+
         public string name { get; set; }
         public string description { get; set; }
 
         public PaymentOptions(string name, string description)
         {
+            
             this.name = name;
             this.description = description;
-        }
-
-        PaymentOptions paymentOptionsOne = new PaymentOptions("Klarna", "Lån");
-        PaymentOptions paymentOptionsTwo = new PaymentOptions("Mastercard", "Kredit");
-        PaymentOptions paymentOptionsThree = new PaymentOptions("Bankkonto", "Tillgångar");
-
-        public string PayInfoOne
-        {
-            get { return paymentOptionsOne.PayInfoOne; }
-
-        }
-        public string PayInfoTwo
-        {
-            get { return paymentOptionsTwo.PayInfoTwo; }
-        }
-        public string PayInfoThree
-        {
-            get { return paymentOptionsThree.PayInfoThree; }
-        }
-
+            
+        }  
     }
 
 
