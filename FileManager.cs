1758965030b773f4a4adfead48937911b2359385
Linneadb <linnea.damber@gmail.com>
2022-11-29 13:33:01 +0100

    Chore: Implemented userList, availableItemsList and Array[1] loggedInUser in Main Window. Lists are passed to CreateUser and OrderWindow through constructors.

--- FileManager.cs
+++ FileManager.cs
@@ -44,46 +44,5 @@ namespace TheStore
                
             File.WriteAllText(path, line);
         }
-
-
-        private void editItemInFile(Item item, int quantityToBuy, int index) //Tar en item, hur många som ska dras bort, och vilken plats i listan/textfilen den har.
-        {
-            int Currentquantity = item.Quantity;
-            if (Currentquantity >= quantityToBuy)
-            {
-                item.Quantity = Currentquantity - quantityToBuy; //Uppdaterar antalet i objektet.
-
-                string name = item.Name;
-                string description = item.Description;
-                int quantity = item.Quantity;
-                int price = item.Price;
-                double weight = item.Weight;
-                string category = item.Category;
-                string itemLines = name + "," + description + "," + quantity + "," + price + "," + weight + "," + category; //En ny string som ska ersätta raden i textfilen
-
-                lineChanger(itemLines, index); // Uppdaterar textfilen. 
-
-                if (item.Quantity == 0) //Om saldot på item blir noll tas den bort från listan och från textfilen
-                {
-                    list.RemoveInAvailableItemsList(item);
-                    lineChanger(null, index);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Not enough " + item.Name + " in store" + Environment.NewLine + "Current balance is: " + Currentquantity);
-            }
-        }
-        static void lineChanger(string newText, int line_to_edit)
-        {
-            DirectoryInfo currentdirectory = new DirectoryInfo(".");
-            string itemPath = currentdirectory.FullName + "\\Files" + @"\Items.txt";
-            string[] arrLine = File.ReadAllLines(itemPath); //Hämtar hela textfilen och läser in varje rad som en egen string i en array.
-            string[] trimmed = arrLine.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray(); //Trimmar bort den första tomma raden om ett saldo skulle gå ner till noll.
-            trimmed[line_to_edit] = newText; //Ändrar den stringen i arrayen som motsvarar platsen för den item vi vill ändra
-            File.WriteAllLines(itemPath, trimmed); //Skriver en ny textfil med arrayen vi skapade två rader upp.
-        }
-
-
     }
 }
